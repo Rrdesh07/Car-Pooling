981aa728ee99ee7c57677d05e4df336f687524c8
Rrdesh07 <rohandeshmukh998@gmail.com>
2024-06-22 18:49:20 +0530

    added routes in carController

--- Server/CarPooling/src/main/java/com/Rrdesh07/CarPooling/Repository/CarRepository.java
+++ Server/CarPooling/src/main/java/com/Rrdesh07/CarPooling/Repository/CarRepository.java
@@ -4,6 +4,8 @@ import com.Rrdesh07.CarPooling.Models.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CarRepository extends JpaRepository<Car,Long> {
 }
