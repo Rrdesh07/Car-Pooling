44f46a9ea7f1c22eadb5e0cb2c5104e457e0a79e
Rrdesh07 <rohandeshmukh998@gmail.com>
2024-06-22 15:40:45 +0530

    changed database to h2 memory database

--- Server/CarPooling/src/main/java/com/Rrdesh07/CarPooling/Models/User.java
+++ Server/CarPooling/src/main/java/com/Rrdesh07/CarPooling/Models/User.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "UserData")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
