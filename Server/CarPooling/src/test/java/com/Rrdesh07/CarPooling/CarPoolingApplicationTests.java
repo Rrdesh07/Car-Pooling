59d362eba6dd176b9077a25084b1f99ea86e2fb7
Rrdesh07 <rohandeshmukh998@gmail.com>
2024-06-22 11:11:40 +0530

    project initialized

---
+++ Server/CarPooling/src/test/java/com/Rrdesh07/CarPooling/CarPoolingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rrdesh07.CarPooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPoolingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
