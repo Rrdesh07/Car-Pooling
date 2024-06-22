13de4cac7929bf3316a0f68cf48c685a0b317094
Rrdesh07 <rohandeshmukh998@gmail.com>
2024-06-22 13:09:28 +0530

    working on userController

--- Server/CarPooling/src/main/java/com/Rrdesh07/CarPooling/Controller/user/UserController.java
+++ Server/CarPooling/src/main/java/com/Rrdesh07/CarPooling/Controller/user/UserController.java
@@ -1,7 +1,15 @@
 package com.Rrdesh07.CarPooling.Controller.user;
 
+import com.Rrdesh07.CarPooling.Models.User;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("api/v1.0/users")
 public class UserController {
+    public List<User> getUsers(){
+        return null;
+    }
 }
