eba9cc67ba87b43150b30a5a02c235e599502c05
Rrdesh07 <rohandeshmukh998@gmail.com>
2024-06-23 18:07:30 +0530

    working on passenger components

--- Client/car-pooling/src/app/app-routing.module.ts
+++ Client/car-pooling/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule,} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { SearchComponent } from './components/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"login",component:LoginComponent
+  },
+  {
+    path:"sign-up",component:SignupComponent
+  },
+  {
+    path:"about",component:AboutComponent
+  },
+  {
+    path:'contact',component:ContactComponent
+  },
+  {
+    path:'search',component:SearchComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Client/car-pooling/src/app/app.module.ts
+++ Client/car-pooling/src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { SearchComponent } from './components/search/search.component';
+import { AllRequestsComponent } from './components/all-requests/all-requests.component';
+import { PasengerRequestsComponent } from './component/pasenger-requests/pasenger-requests.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    AboutComponent,
+    ContactComponent,
+    SearchComponent,
+    AllRequestsComponent,
+    PasengerRequestsComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
